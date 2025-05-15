2b8551ce5424190fa381911de6f00f8acf845939
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2025-05-14 22:46:49 -0300

    Primeiro commit

---
+++ fonte/grafo.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <math.h>
+
+unsigned **criarGrafo(int tam)
+{
+    unsigned **grafo = malloc(tam*sizeof(unsigned*));
+    for(int i = 0; i < tam; i++)
+        grafo[i] = calloc(tam,sizeof(unsigned));
+
+    return grafo;
+}
+
+void liberarGrafo(void **grafo, int tam)
+{
+    for(int i = 0; i < tam; i++)
+        free(grafo[i]);
+    free(grafo);
+}
+
+double **inverterArestas(unsigned **grafoInt, int tam)
+{
+    double **grafoDouble = malloc(tam*sizeof(double*));
+    for(int i = 0; i < tam;i++)
+        grafoDouble[i] = malloc(tam*sizeof(double));
+    
+    for(int i = 0; i < tam; i++)
+        for(int j = 0; j < tam; j++)
+            grafoDouble[i][j] = (grafoInt[i][j] > 0) ? 1.0 / grafoInt[i][j] : INFINITY;
+    return grafoDouble;
+}
---
+++ makefile
@@ -0,0 +1,26 @@
+NOME_ARQ = grafo.exe
+ARQS_C = $(wildcard ./fonte/*.c)
+ARQS_H = $(wildcard ./fonte/*.h)
+
+ARQS_O = $(subst .c,.o,$(subst ./fonte,./objetos,$(ARQS_C)))
+
+CC = gcc
+FLAGS = -O3
+
+all: fonte objetos $(NOME_ARQ)
+
+$(NOME_ARQ): $(ARQS_O)
+	$(CC) $(FLAGS) $^ -o $@
+
+./objetos/main.o: ./fonte/main.c $(ARQS_H)
+	$(CC) $(FLAGS) $< -c -o $@ 
+
+./objetos/%.o : ./fonte/%.c ./fonte/%.h
+	$(CC) $(FLAGS) $< -c -o $@
+
+objetos:
+	mkdir objetos
+
+clean:
+	del $(NOME_ARQ)
+	rmdir /S /Q objetos
